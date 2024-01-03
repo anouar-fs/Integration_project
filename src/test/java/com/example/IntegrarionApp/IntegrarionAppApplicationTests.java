2654ea4df5a3ddf7a4aa12a9c66648abae21d54a
anouar-fs <98779041+anouar-fs@users.noreply.github.com>
2024-01-03 14:44:06 +0100

    first commit

---
+++ src/test/java/com/example/IntegrarionApp/IntegrarionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntegrarionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrarionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
